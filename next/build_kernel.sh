678f90567df5b1d6f539d2faf7692bac348e875a
Lucy <87684856+itslucylol@users.noreply.github.com>
2026-07-01 23:04:33 -0400

    attempt kernel 7 support

--- next/build_kernel.sh
+++ next/build_kernel.sh
@@ -5,6 +5,8 @@ if [ -d "$DIR/kernel" ]; then
     cd /usr/src/linux || exit 1
     make olddefconfig
     make -j$(nproc)
+    make modules_install
+    cp arch/arm64/boot/Image /boot/Image
 else
     echo "Unable to find $DIR"
 fi
